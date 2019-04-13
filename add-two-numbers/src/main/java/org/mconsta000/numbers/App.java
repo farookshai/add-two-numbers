2bb79c7bcc9c01cfa9710204d1195c74522b8d35
mcosnta000 <mconsta000@gmail.com>
2019-04-13 19:48:07 -0400

    Initinal impl.

--- add-two-numbers/src/main/java/org/mconsta000/numbers/App.java
+++ add-two-numbers/src/main/java/org/mconsta000/numbers/App.java
@@ -11,6 +11,41 @@ package org.mconsta000.numbers;
 public class App 
 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+
+        ListNode currentL1 = l1;
+        ListNode currentL2 = l2;
+        ListNode root = new ListNode(0);
+        ListNode current = root;
+        boolean moreNodes = true;
+
+        int carry = 0;
+        while (moreNodes) {
+            int n1 = 0;
+            int n2 = 0;
+            if (currentL1 != null){
+                n1 = currentL1.val;
+                currentL1 = currentL1.next;
+            }
+
+            if (currentL2 != null){
+                n2 = currentL2.val;
+                currentL2 = currentL2.next;
+            }
+
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+
+            ListNode sumNode = new ListNode(sum % 10);
+            current.next = sumNode;
+            current = current.next;
+
+            moreNodes = (currentL1 != null || currentL2 != null);
+        }
+
+        if (carry != 0){
+            current.next = new ListNode(carry);
+        }
+
+        return root.next;
     }
 }
