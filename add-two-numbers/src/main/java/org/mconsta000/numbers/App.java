5dc98189db166fa22598c35f5af343f911158eb2
mcosnta000 <mconsta000@gmail.com>
2019-04-13 17:57:45 -0400

    Project setup

---
+++ add-two-numbers/src/main/java/org/mconsta000/numbers/App.java
@@ -0,0 +1,16 @@
+package org.mconsta000.numbers;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class App 
+{
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
