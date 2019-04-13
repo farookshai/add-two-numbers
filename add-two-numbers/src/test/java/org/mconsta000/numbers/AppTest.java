874875bee6bcb36c109a882de4fa880f961072c4
mcosnta000 <mconsta000@gmail.com>
2019-04-13 19:21:58 -0400

    Create initial unit test

--- add-two-numbers/src/test/java/org/mconsta000/numbers/AppTest.java
+++ add-two-numbers/src/test/java/org/mconsta000/numbers/AppTest.java
@@ -1,6 +1,7 @@
 package org.mconsta000.numbers;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
@@ -9,12 +10,38 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+    private App app = new App();
+
+    public ListNode buildList(int numbers[]){
+        ListNode root = new ListNode(numbers[0]);
+        ListNode current = root;
+        for (int i=1; i<numbers.length; i++){
+            ListNode next = new ListNode(numbers[i]);
+            current.next = next;
+            current = next;
+        }
+        return root;
+    }
+
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void addTwo()
     {
-        assertTrue( true );
+        int n1[] = {2,4,3};
+        int n2[] = {5,6,4};
+        int answer[] = {7, 0, 8};
+
+        ListNode ln1 = buildList(n1);
+        ListNode ln2 = buildList(n2);
+        ListNode result = app.addTwoNumbers(ln1, ln2);
+
+        ListNode current = result;
+        for (int i=0; i<answer.length; i++){
+            assertNotNull(current);
+            assertEquals(answer[i], current.val);
+            current = current.next;
+        }
     }
 }
